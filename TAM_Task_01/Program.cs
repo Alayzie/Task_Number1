6b705036d1055ba1aab114319d6da66ecceb8a48
Aleksey <74556949+Alayzie@users.noreply.github.com>
2020-11-17 13:31:44 +0300

    Task_Number1

---
+++ TAM_Task_01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace TAM_Task_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  
+
+            int coins;
+            int crystalPrice = 15;
+            int buyCrystal;
+            bool Purchase;
+
+            Console.Write("_________________________________________________________________________");
+            Console.Write("\n=========================================================================");
+            Console.Write("\nВведите количество имеющихся монет - ");
+            coins = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write($"Цена кристалла {crystalPrice} монет. Сколько вы желаете приобрести кристаллов? - ");
+            buyCrystal = Convert.ToInt32(Console.ReadLine());
+
+            Purchase = coins >= crystalPrice * buyCrystal;
+            buyCrystal *= Convert.ToInt32(Purchase);
+            coins -= crystalPrice * buyCrystal;
+
+            Console.WriteLine($"У вас в осталось {coins} монет и появилось {buyCrystal} кристаллов.");
+            Console.Write("=========================================================================");
+            Console.Write("\n_________________________________________________________________________");
+
+        }
+    }
+
+}
